c52d88d3670b4400b50eff6d543a109e631ee7f4
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-12-28 10:00:00 +0530

    Initialize Portfolio setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['192.168.56.1'],
+  images: {
+    remotePatterns: [],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
